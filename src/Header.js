cdccad6dc298ba6de72a87ee4ee7a724b7797cb6
eejdan <65033345+eejdan@users.noreply.github.com>
2023-04-03 15:57:58 +0300

    initial

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import './Header.css'
+
+function Header() {
+    return (
+        <div id='header' className="header-wrapper">
+            <div class="header-container">
+                <div>Left</div>
+                <div>Center</div>
+                <div>Right</div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
